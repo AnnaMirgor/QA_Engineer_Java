dbdc0ab8e1b3d851b8dd23fe4e359ffa6102700f
AnnaMirgor <amirgorodskaya@bk.ru>
2026-02-01 19:59:32 +0300

    Task3

--- src/Task2.java
+++ src/Task2.java
@@ -7,11 +7,11 @@ public class Task2 {
 
         var reader = new BufferedReader(new InputStreamReader(System.in));
         System.out.println("Введите первую строку");
-        String string1 = reader.readLine();
+        String a = reader.readLine();
         System.out.println("Введите вторую строку");
-        String string2 = reader.readLine();
+        String b = reader.readLine();
 
-        if (string1.equals(string2)) System.out.println("Строки идентичны");
-        else System.out.println("Строки неидентичны");
+        if (a.equals(b)) System.out.println("Строки идентичны");
+        else System.out.println("Строки не идентичны");
     }
 }
