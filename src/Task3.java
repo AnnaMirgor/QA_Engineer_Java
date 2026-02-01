dfe5704cbbbd707248d57786392e3b753488d043
AnnaMirgor <amirgorodskaya@bk.ru>
2026-02-01 19:56:55 +0300

    Task3

--- src/Task3.java
+++ src/Task3.java
@@ -1,3 +1,10 @@
 public class Task3 {
+    public static void main(String[] args) {
+        int[] mas = {1,2,3,4,5,6,7,8,9,10};
+        for (int item : mas) {
+            if (item%2==0) {
+            System.out.println(item);}
+        }
+    }
 
 }
