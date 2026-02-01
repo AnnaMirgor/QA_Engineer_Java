197bd237d12f10452f85727c7abb7744189178a3
AnnaMirgor <amirgorodskaya@bk.ru>
2026-02-01 19:40:14 +0300

    Task1

---
+++ src/Task1.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+
+public class Task1 {
+    public static void main(String[] args) {
+
+        double a;
+        double b;
+
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Введите целое число a:");
+        a = sc.nextDouble();
+
+        System.out.print("Введите целое число b:");
+        b = sc.nextDouble();
+
+            if (a==b) {
+            System.out.println("a=b");}
+            else {
+                if (a > b) {
+                    System.out.println("a>b");}
+                    else System.out.println("a<b");}
+
+        double sum;
+        double reduce;
+        double mult;
+        double div;
+        sum = a+b;
+        reduce = a-b;
+        mult = a*b;
+        div = a/b;
+
+        System.out.printf("Результат сложения: %f\nРезультат вычитания: %f\n" +
+                "Результат умножения: %f\nРезультат деления: %f", sum, reduce, mult, div);
+
+    }
+
+}
